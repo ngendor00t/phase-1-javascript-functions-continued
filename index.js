3d07044664be17168589bc2aa0e6a7271c898ac0
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 08:23:36 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(Saturday = "roller-skate"){
+    return `This Saturday, I want to ${Saturday}!`;
+    }
+    saturdayFun("bathe my dog")
+    
+    function mondayWork(work = "go to the office"){
+        return `This Monday, I will ${work}.`;
+    }
+    mondayWork("work from home");
+    
+    
+    function wrapAdjective(wrapper){
+        return function(adjective) {
+            return `You are ${wrapper}${adjective}${wrapper}!`;
+        };
+    }
+    
